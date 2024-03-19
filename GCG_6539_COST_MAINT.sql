4f1254a77cc819e6f709803f9776140e33390a4d
Service Web <sw@gssmail.com>
2024-03-19 09:26:04 -0500

    Commit changes in C:\Websites\Git\emarlow\6539\GCG_6539_COST_MAINT.sql

---
+++ GCG_6539_COST_MAINT.sql
@@ -0,0 +1,13 @@
+-- Database: GLOBALPLA Metadata version: 2 Exported: Nov 8, 2021
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_6539_COST_MAINT"  (
+ "VC" DOUBLE,
+ "FRAP" DOUBLE,
+ "FRAS" DOUBLE,
+ "FCWD" DOUBLE,
+ "FCWE" DOUBLE );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
